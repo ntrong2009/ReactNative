7c26ecad06acf5c5f138b299f557f7cdda89eb07
ngoctrong <ngoctrong437@gmail.com>
2021-03-31 23:03:07 +0700

    trong commit

--- src/module/learning/components/index.tsx
+++
@@ -1,9 +0,0 @@
-import Avatar from './learning-youtobe';
-import Cat from './function-component/cat';
-import FlexDirection from './flexbox/flexbox';
-
-export {
-    Avatar,
-    Cat,
-    FlexDirection
-};
